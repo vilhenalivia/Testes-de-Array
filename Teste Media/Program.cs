90c9e1ba56980a64c25e6db5bfc57e68c6c33d8e
Lívia Vilhena <liviavilhena@outlook.com>
2025-01-27 10:58:21 -0300

    Add files via upload

---
+++ Teste Media/Program.cs
@@ -0,0 +1,29 @@
+﻿Console.WriteLine("== Teste de Array com Média ==\n");
+
+
+TestaArrayInt();
+
+void TestaArrayInt()
+{
+    int[] idades = new int[5];
+
+    idades[0] = 30;
+    idades[1] = 40;
+    idades[2] = 17;
+    idades[3] = 21;
+    idades[4] = 18;
+
+    Console.WriteLine($"Tamanho do Array:{idades.Length}");
+
+    // Para calcular a média 
+    int acumulador = 0;
+    for (int i = 0; i < idades.Length; i++)
+    {
+        int idade = idades[i];
+        Console.WriteLine($"Indice: [{i}] = {idade}");
+        acumulador += idade;
+    }
+
+    int media = acumulador / idades.Length;
+    Console.WriteLine($"Média de idades = {media}");
+}
