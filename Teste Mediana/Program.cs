f0e70248ced896bfd6807d58536bafcc38980fd1
Lívia Vilhena <liviavilhena@outlook.com>
2025-01-27 10:59:57 -0300

    Add files via upload

---
+++ Teste Mediana/Program.cs
@@ -0,0 +1,32 @@
+﻿
+// Calculo Mediana.
+Array amostra = Array.CreateInstance(typeof(double), 5);
+amostra.SetValue(5.9, 0);
+amostra.SetValue(1.8, 1);
+amostra.SetValue(7.1, 2);
+amostra.SetValue(10, 3);
+amostra.SetValue(6.9, 4);
+
+TestaMediana(amostra);
+void TestaMediana(Array array)
+{
+    if ((array == null) || (array.Length == 0))
+    {
+        Console.WriteLine("Array para cálculo mediana está vazio ou nulo");
+    }
+
+    // Cópia do Array anterior
+    double[] numerosOrdenados = (double[])array.Clone();
+    // Ordena o Array 
+    Array.Sort(numerosOrdenados);
+
+    //Calculo para a mediana
+    int tamanho = numerosOrdenados.Length;
+    int meio = tamanho / 2;
+    // Se for divisivel por 2 (par), é pego o numero do meio do array , se não, é pego o numero do meio - 1 e divide por 2
+    double mediana = (tamanho % 2 != 0) ? numerosOrdenados[meio] : (numerosOrdenados[meio] + numerosOrdenados[meio - 1]) / 2;
+
+    Console.WriteLine($"Com base na amostra a mediana = {mediana}.");
+
+
+}
---
+++ Teste Pesquisa Palavras/Program.cs
@@ -0,0 +1,27 @@
+﻿
+TestaBuscaPalavras();
+
+void TestaBuscaPalavras()
+{
+    string[] arrayDePalavras = new string[5];
+
+    for (int i = 0; i < arrayDePalavras.Length; i++)
+    {
+        Console.Write($"Digite {i + 1} a palavra: ");
+        arrayDePalavras[i] = Console.ReadLine();
+    }
+
+    Console.Write("Digite a palavra a ser encontrada: ");
+    var busca = Console.ReadLine();
+
+    foreach (string item in arrayDePalavras)
+    {
+        if (item.Equals(busca))
+        {
+            Console.WriteLine($"Palavra encontrada = {busca}");
+            break;
+        }
+
+    }
+
+}
